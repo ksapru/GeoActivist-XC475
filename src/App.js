99a3e0f6168d3ee45fe1b3ae75309c8dd3b3c5fc
Krish Sapru <67235656+ksapru@users.noreply.github.com>
2021-10-06 17:05:19 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import './components/Navbar.js';
+import Navbar from './components/Navbar'
+import React, { useRef, useEffect, useState } from 'react';
+import maptest from './assets/maptest.jpeg'
+ 
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Navbar></Navbar>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Events.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Form from 'form';
+import Col from 'react-bootstrap/Col';
+import Nav from 'react-bootstrap/Nav';
+import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
+
+
+export default function Events() {
+    return (
+        <div>
+              
+        </div>
+    )
+}
