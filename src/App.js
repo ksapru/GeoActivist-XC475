24a0b51f7f66bd793b0709e268ac03c10176822e
ScarlettLi <scartt42@gmail.com>
2021-10-10 17:57:39 -0400

    google mapapi

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import React, { useRef, useEffect, useState, Component } from 'react';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
+
 import './App.css';
 import './components/Navbar.js';
+import './components/Map.css';
 import Navbar from './components/Navbar'
-import React, { useRef, useEffect, useState } from 'react';
 import maptest from './assets/maptest.jpeg'
  
 
@@ -15,4 +18,33 @@ function App() {
   );
 }
 
-export default App;
+// export default App;
+
+const mapStyles = {
+  width: '100%',
+  height: '100%'
+};
+
+export class MapContainer extends Component {
+  render() {
+    return (
+      <Map
+        google={this.props.google}
+        zoom={14}
+        style={mapStyles}
+        initialCenter={
+          {
+            lat: -1.2884,
+            lng: 36.8233
+          }
+        }
+      />
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyCfHoJqKmYyLukVh7qMwz_bRGE5olKcHG4'
+})(MapContainer);
+
+//google dev console
