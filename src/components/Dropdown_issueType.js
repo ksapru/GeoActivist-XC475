d705e0cb8c6076ee81eec4b1225cf19aef086f9d
Krish Sapru <ksapru@bu.edu>
2021-10-12 15:55:34 -0400

    Update Dropdown_issueType.js

--- src/components/Dropdown_issueType.js
+++ src/components/Dropdown_issueType.js
@@ -1,6 +1,6 @@
 import DropdownButton from 'react-bootstrap/DropdownButton'
 import Dropdown from 'react-bootstrap/Dropdown'
-
+import Form from 'react-bootstrap/Form';
 export default function DropDown_issueType(){
     return(
 
@@ -10,12 +10,12 @@ export default function DropDown_issueType(){
         </Dropdown.Toggle>
 
         <Dropdown.Menu>
-            <Dropdown.Item href="#/action-1">Climate Change</Dropdown.Item>
-            <Dropdown.Item href="#/action-2">Civil Rights and Racial Discrimination</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">Immigration Policy</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">Gender Inequality</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">Gun Violence</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">Hunger and Food Insecurity</Dropdown.Item>
+            <Dropdown.Item href="#/action-1"> <Form.Check aria-label="option 1"/>Climate Change</Dropdown.Item>
+            <Dropdown.Item href="#/action-2"> <Form.Check aria-label="option 1"/>Civil Rights and Racial Discrimination</Dropdown.Item>
+            <Dropdown.Item href="#/action-3"> <Form.Check aria-label="option 1"/>Immigration Policy</Dropdown.Item>
+            <Dropdown.Item href="#/action-3"> <Form.Check aria-label="option 1"/>Gender Inequality</Dropdown.Item>
+            <Dropdown.Item href="#/action-3"> <Form.Check aria-label="option 1"/>Gun Violence</Dropdown.Item>
+            <Dropdown.Item href="#/action-3"> <Form.Check aria-label="option 1"/>Hunger and Food Insecurity</Dropdown.Item>
             
         </Dropdown.Menu>
         </Dropdown>
