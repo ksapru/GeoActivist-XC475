e0eb449b2f7a158bedb9e05da20ab8795ef95998
Krish Sapru <ksapru@bu.edu>
2021-10-12 15:55:31 -0400

    Update Dropdown_typeOfActivism.js

--- src/components/Dropdown_typeOfActivism.js
+++ src/components/Dropdown_typeOfActivism.js
@@ -1,21 +1,25 @@
 import DropdownButton from 'react-bootstrap/DropdownButton'
 import Dropdown from 'react-bootstrap/Dropdown'
+import Form from 'react-bootstrap/Form';
 
 export default function DropDown_typeOfActivism(){
     return(
-
+     
         <Dropdown>
         <Dropdown.Toggle variant="success" id="dropdown-basic">
             Type of Activism
         </Dropdown.Toggle>
-
+      
+       
         <Dropdown.Menu>
-            <Dropdown.Item href="#/action-1">Protest and Demonstrations</Dropdown.Item>
-            <Dropdown.Item href="#/action-2">Letter-Writing and Petitions</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">Speaker Events and Discussion</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">Organizational Meetings</Dropdown.Item>
+            <Dropdown.Item href="#/action-1"><Form.Check aria-label="option 1"/>Protest and Demonstrations</Dropdown.Item>
+            <Dropdown.Item href="#/action-2"> <Form.Check aria-label="option 1"/>Letter-Writing and Petitions</Dropdown.Item>
+            <Dropdown.Item href="#/action-3"> <Form.Check aria-label="option 1"/>Speaker Events and Discussion</Dropdown.Item>
+            <Dropdown.Item href="#/action-3"><Form.Check aria-label="option 1"/> Organizational Meetings</Dropdown.Item>
         </Dropdown.Menu>
+        
+         
         </Dropdown>
-
+         
     );
 }
