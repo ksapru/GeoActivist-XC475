01cc3d96bfa3a634b949ec1b3d0a2f1e93cd9e22
Krish Sapru <ksapru@bu.edu>
2021-11-01 03:18:48 -0400

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,29 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-import Button from 'react-bootstrap/Button';
+ 
 import Container from 'react-bootstrap/container';
 import Navbar from 'react-bootstrap/Navbar'
-import Form from 'react-bootstrap/Form';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+ 
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import Button from "react-bootstrap/Button";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import About from '../About';
 
 
 export default function navbar() {
+
+  
     return (
         <div>
+          
+          <Router>
            <Navbar variant="dark" bg="dark" expand="lg">
   <Container fluid>
     <Navbar.Brand href="#home"> GeoActivist </Navbar.Brand>
@@ -40,11 +51,31 @@ export default function navbar() {
           <NavDropdown.Item href="#action/3.4">Activists near me</NavDropdown.Item>
         </NavDropdown>
       </Nav>
+
+
+      <Nav>
+        <NavDropdown
+          id="nav-d"
+          title="User Login"
+          menuVariant="dark"
+          align="right"
+        >
+          <NavDropdown.Item>
+            <Router>
+              
+              <Button exact component="/About" path={About}><Route path="/" component={About}> Create an Account  </Route></Button>
+              
+            </Router>
+          </NavDropdown.Item>
+          <NavDropdown.Item><Button link to="/About">User Login</Button></NavDropdown.Item>
+        </NavDropdown>
+      </Nav>
+
     </Navbar.Collapse>
   </Container>
 </Navbar>
- 
-            
-        </div>
+</Router>
+  
+  </div>
     )
 }
