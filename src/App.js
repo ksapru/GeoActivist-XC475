8fdcef6f7520b66c9516c9e4aa60feb2d1e4b486
Krish Sapru <ksapru@bu.edu>
2021-11-01 03:18:47 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -30,76 +30,30 @@ import DropDown_issueType from './components/Dropdown_checkBox.js';
 import './components/Map.js'
 import Map from './components/Map'
 
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
 import './components/CustomMarker.js'
 import CustomMarker from './components/CustomMarker'
 
+
+import About from './About'
 // import './components/CustomMarker'
 // import CustomMarker from './components/CustomMarker'
 mapboxgl.accessToken = 'pk.eyJ1IjoidHphbGl5YSIsImEiOiJja3VuMncxd3QzeHI3MnZtbmZyOTE0Z2RhIn0.wXgglO-cXtCIq-QJ17Jv-g';
 
-
 function App() {
-
   const mapContainer = useRef(null);
   const map = useRef(null);
   const [lng, setLng] = useState(-71.1029);
   const [lat, setLat] = useState(42.3486);
   const [zoom, setZoom] = useState(15.48);
-
-
-  // const TOKEN='pk.eyJ1IjoidHphbGl5YSIsImEiOiJja3VuMncxd3QzeHI3MnZtbmZyOTE0Z2RhIn0.wXgglO-cXtCIq-QJ17Jv-g';
-
-  // const geolocateStyle = {
-  //   float: 'left',
-  //   margin: '50px',
-  //   padding: '10px'
-  // };
-
-  // const Map = () => {
-
-  // const [viewport, setViewPort ] = useState({
-  //   width: "100%",
-  //   height: 900,
-  //   latitude: 0,
-  //   longitude: 0,
-  //   zoom: 2
-  // })
-
-  // const _onViewportChange = viewport => setViewPort({...viewport, transitionDuration: 3000 })
-
-  // useEffect(() => {
-  //   if (map.current) return; // initialize map only once
-  //   map.current = new mapboxgl.Map({
-  //   container: mapContainer.current,
-  //   style: 'mapbox://styles/mapbox/streets-v11',
-  //   center: [lng, lat],
-  //   zoom: zoom
-  //   });
-  //   });
-
-    // useEffect(() => {
-    //   if (!map.current) return; // wait for map to initialize
-    //   map.current.on('move', () => {
-    //   setLng(map.current.getCenter().lng.toFixed(4));
-    //   setLat(map.current.getCenter().lat.toFixed(4));
-    //   setZoom(map.current.getZoom().toFixed(2));
-    //   });
-    //   });
-  
-  // geojson.features.map((features) =>
-  //     new mapboxgl.Marker().setLngLat(features.geometry.coordinates).addTo(map)
-  // );
-  // addControl(
-  //   new mapboxgl.GeolocateControl({
-  //   positionOptions: {
-  //   enableHighAccuracy: true
-  //   },
-  //   // When active the map will receive updates to the device's location as it changes.
-  //   trackUserLocation: true,
-  //   // Draw an arrow next to the location dot to indicate which direction the device is heading.
-  //   showUserHeading: true
-  //   })
-  //   );
 
   return (
     <div className="App">
@@ -107,9 +61,15 @@ function App() {
       <Navbar></Navbar>
 
       <div className="SearchBar">
-        <Searchbar ></Searchbar>
+        <Router>
+          <Switch>
+          <Route path="/" exact component={Searchbar}></Route>
+          <Route path="/About" exact component={About}></Route>
+          </Switch>
+        </Router>
+
         <DropDown_typeOfActivism></DropDown_typeOfActivism>
-        <DropDown_issueType></DropDown_issueType>
+        <DropDown_issueType></DropDown_issueType> 
         {/* <Dropdown_checkBox></Dropdown_checkBox> */}
         <SliderPage></SliderPage>
         {/* <img src={require('./components/mapbox-marker-icon-blue.svg').default}/> */}
@@ -117,11 +77,11 @@ function App() {
       </div>
       <div className="Map">
       <Map></Map>
+      <Searchbar ></Searchbar>
       {/* <CustomMarker></CustomMarker> */}
       </div>
-      
-
     </div>
+
   );
 }
 export default App;
